44863616219701aedd728312a88aefedb833f8f7
Oyedele Hammed Horlah <itz.harmid@gmail.com>
2018-05-20 14:37:03 +0100

    Refactor the code

--- HRouter.js
+++ HRouter.js
@@ -16,27 +16,27 @@ HRouter.prototype = {
   run: function() {
     var self = this,
       go = function() {
-        var url = location.hash.slice(1).split("?"),
-        path = url[0] || "/",
-        queryStr = url[1] + "&" + location.search.slice(1) || "",
+        var url = location.hash.slice( 1 ).split( '?' ),
+        path = url[0] || '/',
+        queryStr = url[ 1 ] + '&' + location.search.slice( 1 ) || '',
         i = 0,
         keys = [];
         for ( ; i < self.routes.length; i++ ) {
-          var reg = RegExp( "^" + self.routes[i].path.replace(/\:(\w+)/g, function( _, key ){
+          var reg = RegExp( '^' + self.routes[i].path.replace( /\:(\w+)/g, function( _, key ){
             keys.push( key );
-            return "([^\/]+)";
-          }) + "(?:\/$|$)" ),
+            return '([^\/]+)';
+          }) + '(?:\/$|$)' ),
           match;
           if ( reg.test( path ) ) {
             match = reg.exec( path );
             var req = { params: {}, query: {} };
-            match.slice(1).map(function( val, idx ){
+            match.slice( 1 ).map( function( val, idx ){
               req.params[ keys[ idx ] ] = val;
-            });
-            queryStr.split("&").map(function( pair ){
-              pair = pair.split("=");
-              req.query[ pair[0] ] = pair[1];
-            });
+            } );
+            queryStr.split( '&' ).map( function( pair ){
+              pair = pair.split( '=' );
+              req.query[ pair[ 0 ] ] = pair[ 1 ];
+            } );
             self.routes[ i ].fn( req );
             break;
           }
